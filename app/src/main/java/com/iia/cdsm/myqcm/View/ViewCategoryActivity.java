24df86494a73fa4d5587f491766b24391586d2b7
alexislepage <alexislepage@hotmail.fr>
2016-06-08 17:06:27 +0200

    Push ViewCategoryActivity

--- app/src/main/java/com/iia/cdsm/myqcm/View/ViewCategoryActivity.java
+++ app/src/main/java/com/iia/cdsm/myqcm/View/ViewCategoryActivity.java
@@ -3,6 +3,7 @@ package com.iia.cdsm.myqcm.View;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import com.iia.cdsm.myqcm.R;
@@ -12,6 +13,10 @@ import com.iia.cdsm.myqcm.data.CategorySQLiteAdapter;
  * Created by Alex on 11/05/2016.
  */
 public class ViewCategoryActivity extends AppCompatActivity {
+
+    private ListView mDrawerList;
+    private ArrayAdapter<String> mAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
